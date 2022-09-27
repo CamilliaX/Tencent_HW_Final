3ea4dbddb18cea000db4693b6f28423355e2c2e2
CAMILLIA <491441320@qq.com>
2022-09-27 21:15:18 +0800

    Add -

---
+++ Source/CoopGame/Private/SPickupActor.cpp
@@ -0,0 +1,69 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SPickupActor.h"
+#include "Components/SphereComponent.h"
+#include "Components/DecalComponent.h"
+#include "SPowerupActor.h"
+#include "TimerManager.h"
+
+// Sets default values
+ASPickupActor::ASPickupActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	//PrimaryActorTick.bCanEverTick = true;
+
+	SphereComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
+	SphereComp->SetSphereRadius(75.0f);
+	RootComponent = SphereComp;
+
+	DecalComp = CreateDefaultSubobject<UDecalComponent>(TEXT("DecalComp"));
+	DecalComp->SetRelativeRotation(FRotator(90, 0.0f, 0.0f));
+	DecalComp->DecalSize = FVector(64, 75, 75);
+	DecalComp->SetupAttachment(RootComponent);
+
+	CooldownDuration = 10.0f;
+
+	SetReplicates(true);
+}
+
+// Called when the game starts or when spawned
+void ASPickupActor::BeginPlay()
+{
+	Super::BeginPlay();
+	if (HasAuthority())
+	{
+		Respawn();
+	}
+}
+void ASPickupActor::Respawn()
+{
+	if (PowerUpClass == nullptr)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PowerUpClass is nullptr in %s. Please update your Blueprint"), *GetName());
+		return;
+	}
+
+	FActorSpawnParameters SpawnParams;
+	SpawnParams.SpawnCollisionHandlingOverride = ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+
+	PowerUpInstance = GetWorld()->SpawnActor<ASPowerupActor>(PowerUpClass, GetTransform(), SpawnParams);
+}
+
+
+void ASPickupActor::NotifyActorBeginOverlap(AActor* OtherActor)
+{
+	Super::NotifyActorBeginOverlap(OtherActor);
+
+	if (HasAuthority() && PowerUpInstance)
+	{
+		PowerUpInstance->ActivatePowerup(OtherActor);
+		PowerUpInstance = nullptr;
+
+		// Set Timer 
+		GetWorldTimerManager().SetTimer(TimerHandle_RespawnTimer, this, &ASPickupActor::Respawn, CooldownDuration);
+	}
+}
+
+
+
